6ccf2893c173aa2ea2d28c5a76503de9146678d4
AbuTuraab <aliyulekan@gmail.com>
2023-11-27 21:31:43 +0100

    finalfinal

--- src/Components/Accordion.jsx
+++ src/Components/Accordion.jsx
@@ -1,5 +1,4 @@
-import { useState } from "react"
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import Robot from "../assets/Robot.png"
 
 const Accordion = () => {
